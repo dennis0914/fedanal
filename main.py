cecaccddf426e0660a0250d0738d84d4982fea1d
dennis0914 <dennis0914@naver.com>
2022-03-15 23:41:41 +0900

    ADD: train and test iterator

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import spacy
 import pandas as pd
 from torchtext.legacy.data import Field, BucketIterator, TabularDataset
 from sklearn.model_selection import train_test_split
+import torch
 
 train_statement = pd.read_json("data/train_statement.json")
 test_statement = pd.read_json("data/test_statement.json")
@@ -19,3 +20,20 @@ train_data, test_data = TabularDataset.splits(
     test = "test_statement.json",
     format = "json",
     fields = fields)
+
+statement.build_vocab(train_data)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+BATCH_SIZE = 1
+
+train_iterator, test_iterator = BucketIterator.splits(
+    (train_data, test_data),
+    sort = False,
+    batch_sizes=BATCH_SIZE,
+    device = device
+)
+
+print('Train: ')
+for batch in train_iterator:
+    print(batch)
